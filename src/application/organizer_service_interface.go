dfa2972832dedaec0795c44979a8a7226184d166
rubenbuelvas <rcbuelvasv@gmail.com>
2026-03-02 07:52:09 -0500

    Restructure DTO and services

--- src/application/organizer_service_interface.go
+++ src/application/organizer_service_interface.go
@@ -1,11 +1,7 @@
 package application
 
-import "github.com/rubenbuelvas/ringover/src/domain"
+import "github.com/rubenbuelvas/game-organizer-be/src/domain"
 
 type IOrganizerService interface {
-	GetProjects() ([]domain.Project, error)
-	GetProjectByID(id int64) (domain.Project, error)
-	CreateProject(data CreateProjectDTO) error
-	UpdateProject(id int64, data UpdateProjectDTO) error
-	DeleteProject(id int64) error
+	Organize(dto OrganizerDTO) ([]domain.Team, error)
 }
